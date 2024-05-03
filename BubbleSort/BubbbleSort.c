b369522a87750316c0283206bb924dcbc5260172
PedroTrento <pedrotrento8@gmail.com>
2024-05-03 09:40:08 -0300

    Random Array

--- BubbleSort/BubbbleSort.c
+++ BubbleSort/BubbbleSort.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 10
 
 int main(){
-    //print
-    printf("Hello World");
+
+    int arr[TAM];
+
+    srand(time(NULL));
+
+    for(int i = 0; i < TAM; i++){
+        arr[i] = rand()%TAM;
+    }
+
+    printf("Array:");
+    for(int i = 0; i < TAM; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+
     return 0;
 }
